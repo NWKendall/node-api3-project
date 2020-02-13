8391d6d3e6ba25437128fde477fa44baef22cf21
nicbongo <nicbongo@protonmail.com>
2020-02-13 18:03:03 -0400

    MVP!

--- users/userRouter.js
+++ users/userRouter.js
@@ -86,18 +86,10 @@ router.get('/:id/posts', (req, res) => {
     })      
 });
 
-// TO DO!
-router.post('/:id/posts', validatePost, (req, res) => {
-  // user creates a new post
-
-  
-});
-
 router.put('/:id', validateUserId, validateUser, (req, res) => {
   // do your magic!
   console.log(`req body 1`,req.body)
   const { id } = req.params;
-
   userData
     .update(id, req.body)
     .then(edit => {
@@ -112,8 +104,23 @@ router.put('/:id', validateUserId, validateUser, (req, res) => {
 
 
 
+// TO DO!
+router.post('/:id/posts', validatePost, (req, res) => {
+  // user creates a new post
+  const { id } = req.params;
+  const post = { ...req.body, user_id: id };  
+  console.log(`BODY from user post`, post)
 
-
+  postData
+    .insert(post)
+    .then(newPost => {          
+          res.status(201).json(newPost)
+        })
+    .catch(err => {
+      console.log(`this is error from new user post`, err)
+      res.status(500).json({ error: "Creating new POST FAILED" })
+    })    
+});
 
 
 //custom middleware
@@ -130,16 +137,17 @@ function validateUser(req, res, next) {
   // validates all POST requests for new user (not new user posts)  
   const { body } = req;
   !body? 
-  res.status(400).json({ message: "missing user data" }) :
-  !body.name ? res.status(400).json({ message: "missing required name field" }) : 
-  next();
- 
+    res.status(400).json({ message: "missing user data" }) :
+    !body.name ? res.status(400).json({ message: "missing required name field" }) : 
+    next();
+  
 }
 
 function validatePost(req, res, next) {
   // validates all POST requests for new post (not new user)
   const { id } = req.params;
   const post = { ...req.body, user_id: id };  
+  console.log(`validate post:`, post)
 
   !post ? 
     res.status(400).json({ message: "missing user data" }) 
